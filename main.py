a088e223143e8c5e04b798d4ddc2c57d7f45ebb4
Colosen <74822930+Colosen@users.noreply.github.com>
2021-09-22 16:16:58 +0530

    Added HP bar, MP bar (static)

--- main.py
+++ main.py
@@ -2,6 +2,21 @@ from Classes.game import Person, bcolors
 from Classes.magic import Spell
 from Classes.inventory import Item
 
+print("\n\n")
+print("NAME                     HP                                      MP")
+print("                         _________________________               __________")
+print(bcolors.BOLD + "Valos:          " +
+      "460/460 |" + bcolors.OKGREEN + "█████████████            " + bcolors.ENDC +
+      bcolors.BOLD + "|       65/65 |" + bcolors.OKBLUE + "██████████" + bcolors.ENDC + "|")
+
+print("                         _________________________               __________")
+print("Valos:          460/460 |                         |       65/65 |          |")
+
+print("                         _________________________               __________")
+print("Valos:          460/460 |                         |       65/65 |          |")
+
+print("\n\n")
+
 # Create black magic
 fire = Spell("Fire", 10, 100, "black")
 thunder = Spell("Thunder", 10, 100, "black")
